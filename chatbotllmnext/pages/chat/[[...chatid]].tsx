79fc01f63589582a3e4bae48a6d4e4e7fedc29f9
unknown <mynamerahulkumar13@gmail.com>
2024-12-18 12:09:34 +0000

    updated nextjs

---
+++ chatbotllmnext/pages/chat/[[...chatid]].tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next JS ChatGPT Starter</title>
+      </Head>
+      <h1>Welcome to the Next JS &amp; ChatGPT Starter</h1>
+    </div>
+  );
+}
